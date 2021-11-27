af3c8940427f6464ba0bc3158d37ec59eb7e8256
Hetvi Patel <92144723+Hetvi415@users.noreply.github.com>
2021-11-28 00:39:55 +0530

    Create sellerdetail.js

---
+++ sellerdetail.js
@@ -0,0 +1,17 @@
+const seller = [
+    {
+        "seller_id":"1",
+        "seller_name":"Hetvi",
+        "pid":"1"
+    },
+    {
+        "seller_id":"2",
+        "seller_name":"Hetu",
+        "pid":"2"
+    },
+    {
+        "seller_id":"3",
+        "seller_name":"Htv",
+        "pid":"3"   
+    }
+];
