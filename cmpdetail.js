5b5a1f50f657479b900a1f3dc7cee1569771e8c8
Hetvi Patel <92144723+Hetvi415@users.noreply.github.com>
2021-11-28 00:33:49 +0530

    Create cmpdetail.js

---
+++ cmpdetail.js
@@ -0,0 +1,18 @@
+const cmp = [
+    {
+        "cpmid":"1",
+        "cmpname":"ABC",
+        "pid":"101"
+    },
+    {
+        "cpmid":"2",
+        "cmpname":"XYZ",
+        "pid":"102"        
+    },
+    {
+        "cpmid":"3",
+        "cmpname":"PQR",
+        "pid":"103"
+    }
+    
+];
