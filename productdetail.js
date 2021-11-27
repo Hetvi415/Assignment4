3532412cddcf69b5e7b4eefbedbbf07396c403d9
Hetvi Patel <92144723+Hetvi415@users.noreply.github.com>
2021-11-28 00:39:26 +0530

    Create productdetail.js

---
+++ productdetail.js
@@ -0,0 +1,28 @@
+const product = [
+    {
+        "p_id":"1",
+        "p_title":"Bat",
+        "price":"100",
+        "p_cat":["toy","food","animal"],
+        "cmp_id":"1",
+        "seller_id":["001","002"]
+    },
+    {
+        "p_id":"2",
+        "p_title":"Ball",
+        "price":"50",
+        "p_cat":["toy","food","animal"],
+        "cmp_id":"2",
+        "seller_id":["007","009"] 
+    },
+    {
+        "p_id":"3",
+        "p_title":"rackets",
+        "price":"399",
+        "p_cat":["toy","food","animal"],
+        "cmp_id":"3",
+        "seller_id":["004","005"]
+    }
+];
+
+module.exports = product;
